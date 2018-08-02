a63254c42ac213c4e99fb1576d7fa629ddd4afe6
Voriah <rodgick@yahoo.com>
2018-08-01 21:22:19 -0700

    ok redo

--- injectable.js
+++ injectable.js
@@ -1,291 +1,179 @@
-var l = document.getElementById("lb");
-var k = document.getElementById("kg");
-var r = document.getElementById("dRange1");
-var s1 = document.getElementById("sig1");
-var s2 = document.getElementById("sig2");
-var s3 = document.getElementById("sig3");
-var s4 = document.getElementById("sig4");
-var s5 = document.getElementById("sig5");
-var s6 = document.getElementById("sig6");
-var s7 = document.getElementById("sig7");
-var dose = 10;
-var conc = 10;
-var weight = 10;
+//calculates weight and converts to kilograms if set in pounds
+function calcWeight() {
+  var weight = icon_weight.value;
+  if (lb.checked) {      //convert pounds to kg
+    weight /= 2.2;
+  }
+  else if (kg.checked) {
+    weight = weight;
+  }
+  else {
+    alert("Specify weight type.");
+    return;
+  }
+  return weight;
+}
 
-
+//finds next available list item to populate
 function setSig(a, b, c) {
-  if (s1.style.display === "none") {
+  if (sig1.style.display === "none") {
     setSig1(a, b, c);
   }
-  else if (s2.style.display === "none") {
+  else if (sig2.style.display === "none") {
     setSig2(a, b, c);
   }
-  else if (s3.style.display === "none") {
+  else if (sig3.style.display === "none") {
     setSig3(a, b, c);
   }
-  else if (s4.style.display === "none") {
+  else if (sig4.style.display === "none") {
     setSig4(a, b, c);
   }
-  else if (s5.style.display === "none") {
+  else if (sig5.style.display === "none") {
     setSig5(a, b, c);
   }
-  else if (s6.style.display === "none") {
+  else if (sig6.style.display === "none") {
     setSig6(a, b, c);
   }
-  else if (s7.style.display === "none") {
-    setSig7(a, b, c);
+}
+//checks the next available range object
+function ranges() {
+  if (sig1.style.display === "none") {
+    return dRange1;
+  }
+  else if (sig2.style.display === "none") {
+    return dRange2;
+  }
+  else if (sig3.style.display === "none") {
+    return dRange3;
+  }
+  else if (sig4.style.display === "none") {
+    return dRange4;
+  }
+  else if (sig5.style.display === "none") {
+    return dRange5;
+  }
+  else if (sig6.style.display === "none") {
+    return dRange6;
   }
 }
-function ranges(r) {
-  if (s1.style.display === "none") {
-    window.r = document.getElementById("dRange1");
-  }
-  else if (s2.style.display === "none") {
-    window.r = document.getElementById("dRange2");
-  }
-  else if (s3.style.display === "none") {
-    window.r = document.getElementById("dRange3");
-  }
-  else if (s4.style.display === "none") {
-    window.r = document.getElementById("dRange4");
-  }
-  else if (s5.style.display === "none") {
-    window.r = document.getElementById("dRange5");
-  }
-  else if (s6.style.display === "none") {
-    window.r = document.getElementById("dRange6");
-  }
-  else if (s7.style.display === "none") {
-    window.r = document.getElementById("dRange7");
-  }
-}
-
+//assigns values to list items
 function setSig1(a, b, c) {
   sigName1.innerHTML = a;
   dose1.innerHTML = b;
   mgkg1.innerHTML = c;
-  s1.style.display = "flex";
+  sig1.style.display = "flex";
 }
 function setSig2(a, b, c) {
   sigName2.innerHTML = a;
   dose2.innerHTML = b;
   mgkg2.innerHTML = c;
-  s2.style.display = "flex";
+  sig2.style.display = "flex";
 }
 function setSig3(a, b, c) {
   sigName3.innerHTML = a;
   dose3.innerHTML = b;
   mgkg3.innerHTML = c;
-  s3.style.display = "flex";
+  sig3.style.display = "flex";
 }
 function setSig4(a, b, c) {
   sigName4.innerHTML = a;
   dose4.innerHTML = b;
   mgkg4.innerHTML = c;
-  s4.style.display = "flex";
+  sig4.style.display = "flex";
 }
 function setSig5(a, b, c) {
   sigName5.innerHTML = a;
   dose5.innerHTML = b;
   mgkg5.innerHTML = c;
-  s5.style.display = "flex";
+  sig5.style.display = "flex";
 }
 function setSig6(a, b, c) {
   sigName6.innerHTML = a;
   dose6.innerHTML = b;
   mgkg6.innerHTML = c;
-  s6.style.display = "flex";
-}
-function setSig7(a, b, c) {
-  sigName7.innerHTML = a;
-  dose7.innerHTML = b;
-  mgkg7.innerHTML = c;
-  s7.style.display = "flex";
+  sig6.style.display = "flex";
 }
 
-function calc(dose, conc, weight) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  return (dose * weight / conc).toFixed(2);
-};
+//constructs injectable objects
+function Injectable(n, d, c, min, max, ste) {
+  this.str = n;
+  this.dose = d;
+  this.conc = c;
+  this.mi = min;
+  this.ma = max;
+  this.st = ste;
+}
+// Name, Dose, Concentration, Minimum Range, Maximum Range, Range Step
+var acep = new Injectable("Acepromazine: 1 mg/ml", 0.05, 1, 0.03, 0.1, 0.01);
+var adeq = new Injectable("Adequan: 100 mg/ml", 4.4, 100, 4, 5, 0.1);
+var amik = new Injectable("Amikacin: 250 mg/ml", 10, 250, 8, 12, 0.5);
+var amin = new Injectable("Aminophylline: 25 mg/ml", 7, 25, 3, 11, 0.5);
+var amox = new Injectable("Amoxicillin: 50 mg/ml", 22, 50, 18, 24, 0.5);
+var ampi = new Injectable("Ampicillin: 100 mg/ml", 22, 100, 18, 24, 0.5);
+var ante = new Injectable("Antesedan: 5 mg/ml", 1, 5, 1, 1, 1);
+var apom = new Injectable("Apomorphine: 3 mg/ml", 0.03, 3, 0.01, 0.05, 0.01);
+var atro = new Injectable("Atropine: 0.54 mg/ml", 0.05, 0.54, 0.01, 0.1, 0.01);
+var bupi = new Injectable("Bupivicaine: 5 mg/ml", 1.5, 5, 1, 2, 0.01);
+var bupr = new Injectable("Buprenorphine: 0.3 mg/ml", 0.015, 0.3, 0.01, 0.02, 0.001);
+var buto = new Injectable("Butorphanol: 10 mg/ml", 0.4, 10, 0.2, 0.6, 0.05);
+var cefa = new Injectable("Cefazolin: 100 mg/ml", 22, 100, 18, 26, 1);
+var cere = new Injectable("Cerenia: 10 mg/ml", 1, 10, 0.5, 1.5, 0.5);
+var clin = new Injectable("Clindamycin: 150 mg/ml", 12.5, 150, 10, 15, 0.5);
+var conv = new Injectable("Convenia: 80 mg/mL:", 8, 80, 6, 10, 0.5);
+var depo = new Injectable("Depo-Medrol: 20 mg/mL:", 1, 20, 1, 1, 1);
+var dexd = new Injectable("Dexdomitor: 0.5 mg/mL:", 1, 0.5, 1, 1, 1);
+var dexa = new Injectable("Dexamethasone: 2 mg/mL:", 0.1, 2, 0.05, 0.15, 0.01);
+var dexaSP = new Injectable("Dexamethasone SP: 4 mg/mL:", 0.25, 4, 0.2, 0.3, 0.01);
+var diaz = new Injectable("Diazepam: 5 mg/mL:", 0.2, 5, 0.1, 0.4, 0.01);
+var diph = new Injectable("Diphenhydramine: 50 mg/mL:", 2.2, 50, 2, 2.4, 0.1);
+var dopa = new Injectable("Dopamine: 40 mg/mL:", 1, 40, 1, 1, 1);
+var doxa = new Injectable("Doxapram: 20 mg/mL:", 5.5, 20, 1, 11, 1);
+var enro = new Injectable("Enrofloxacin: 22.7 mg/mL:", 5, 22.7, 4, 6, 0.1);
+var epin = new Injectable("Epinephrine: 1 mg/mL:", 0.2, 1, 0.1, 0.3, 0.01);
+var famo = new Injectable("Famotidine: 10 mg/mL:", 0.5, 10, 0.3, 0.7, 0.1);
+var flum = new Injectable("Flumazenil: 1 mg/mL:", 1, 1, 1, 1, 1);
+var furo = new Injectable("Furosemide: 50 mg/mL:", 2.2, 50, 2, 2.4, 0.1);
+var gent = new Injectable("Gentamicin: 100 mg/mL:", 10.5, 100, 9, 12, 0.5);
+var hydr = new Injectable("Hydromorphone: 2 mg/mL:", 0.075, 2, 0.05, 0.1, 0.01);
+var iron = new Injectable("Iron Dextran: 100 mg/mL:", 15, 100, 10, 20, 1);
+var kena = new Injectable("Kenalog: 10 mg/mL:", 0.16, 10, 0.11, 0.22, 0.01);
+var keta = new Injectable("Ketamine: 100 mg/mL:", 1, 100, 1, 1, 1);
+var lido = new Injectable("Lidocaine: 20 mg/mL:", 2, 20, 1, 3, 0.1);
+var mani = new Injectable("Mannitol: 200 mg/mL:", 550, 200, 250, 1000, 50);
+var melo = new Injectable("Meloxicam: 5 mg/mL:", 0.2, 5, 0.1, 0.3, 0.01);
+var metho = new Injectable("Methocarbamol: 100 mg/mL:", 1, 100, 1, 1, 1);
+var meto = new Injectable("Metoclopramide: 5 mg/mL:", 0.3, 5, 0.2, 0.4, 0.01);
+var metro = new Injectable("Metronidazole: 250 mg/mL:", 1, 250, 1, 1, 1);
+var mida = new Injectable("Midazolam: 5 mg/mL:", 0.2, 5, 0.1, 0.3, 0.01);
+var nalo = new Injectable("Naloxone: 0.4 mg/mL:", 0.25, 0.4, 0.01, 0.04, 0.005);
+var onda = new Injectable("Ondansetron: 2 mg/mL:", 0.75, 2, 0.5, 1, 0.05);
+var pant = new Injectable("Pantoprozole: 1 mg/mL:", 1, 1, 1, 1, 1);
+var perc = new Injectable("Percorten-V: 25 mg/mL:", 1.8, 25, 1.65, 2.2, 0.05);
+var praz = new Injectable("Praziquantel: 56.8 mg/mL:", 1, 56.8, 1, 1, 1);
+var prop = new Injectable("Propofol: 10 mg/mL:", 4, 10, 2, 6, 0.5);
+var soluD = new Injectable("Solu-Delta-Cortef: 1 mg/mL:", 1, 1, 1, 1, 1);
+var soluM = new Injectable("Solu-Medrol: 62.5 mg/mL:", 1, 62.5, 1, 1, 1);
+var vitb = new Injectable("Vitamin B-12: 1 mg/mL:", 0.025, 1, 0.02, 0.03, 0.001);
 
-function convenia(dose, conc, weight, r) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  
-  window.conc = 80;      //concentration
-  window.dose = 8;       //dosage in mg/kg
- 
-  ranges(r);
 
+//calculates dosage and sets values to next list item that is not visible
+function calc(inject) {
+  var w = calcWeight();  
   
-  window.r.value = window.dose;
-  window.r.step = 0.5;
-  window.r.min = 6;
-  window.r.max = 10;
+  return (inject.dose * w / inject.conc).toFixed(2);
+}
 
-  var amt = (window.dose*window.weight/window.conc);
-  
-  var a = "Convenia: 80 mg/mL:"; 
-  var b = calc(window.dose, window.conc, window.weight);
-  var c = window.dose;
+function calcInjectable(inject) {
+  var r = ranges();
+
+  r.value = inject.dose;
+  r.step = inject.st;
+  r.min = inject.mi;
+  r.max = inject.ma;
+
+  var a = inject.str; 
+  var b = calc(inject);
+  var c = inject.dose;
 
   setSig(a, b, c);
- 
 }
-
-function acepromazine(dose, conc, weight, r) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  
-  window.conc = 1;      //concentration
-  window.dose = 0.05;       //dosage in mg/kg
- 
-  ranges(r);
-
-  
-  window.r.value = window.dose;
-  window.r.step = 0.01;
-  window.r.min = 0.03;
-  window.r.max = 0.1;
-
-  var amt = (window.dose*window.weight/window.conc);
-  
-  var a = "Acepromazine: 1 mg/mL:"; 
-  var b = calc(window.dose, window.conc, window.weight);
-  var c = window.dose;
-
-  setSig(a, b, c);
- 
-}
-
-function adequan(dose, conc, weight, r) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  
-  window.conc = 100;      //concentration
-  window.dose = 4.4;       //dosage in mg/kg
- 
-  ranges(r);
-
-  
-  window.r.value = window.dose;
-  window.r.step = 0.1;
-  window.r.min = 3.9;
-  window.r.max = 4.9;
-
-  var amt = (window.dose*window.weight/window.conc);
-  
-  var a = "Adequan: 100 mg/mL:"; 
-  var b = calc(window.dose, window.conc, window.weight);
-  var c = window.dose;
-
-  setSig(a, b, c);
- 
-}
-
-function amikacin(dose, conc, weight, r) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  
-  window.conc = 250;      //concentration
-  window.dose = 10;       //dosage in mg/kg
- 
-  ranges(r);
-
-  
-  window.r.value = window.dose;
-  window.r.step = 0.5;
-  window.r.min = 8;
-  window.r.max = 12;
-
-  var amt = (window.dose*window.weight/window.conc);
-  
-  var a = "Amikacin: 250 mg/mL:"; 
-  var b = calc(window.dose, window.conc, window.weight);
-  var c = window.dose;
-
-  setSig(a, b, c);
- 
-}
-
-function aminophylline(dose, conc, weight, r, am) {
-  weight = document.getElementById("icon_weight").value;
-  if (l.checked) {      //convert pounds to kg
-    weight /= 2.2;
-  }
-  else if (k.checked) {
-    weight = weight;
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-  
-  window.conc = 25;      //concentration
-  window.dose = 7;       //dosage in mg/kg
- 
-  ranges(r);
-
-  
-  window.r.value = window.dose;
-  window.r.step = 1;
-  window.r.min = 3;
-  window.r.max = 11;
-
-  var amt = (window.dose*window.weight/window.conc);
-  
-  var a = "Aminophylline: 25 mg / mL:"; 
-  var b = calc(window.dose, window.conc, window.weight);
-  var c = window.dose;
-
-  setSig(a, b, c);
- 
-}
-
-
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//shows antibiotic list if not active: hides any others visible, hides if active
 function anti(){
     var x = document.getElementsByClassName("antibiotics");
     for (i = 0; i < x.length; i++) { 
@@ -10,81 +10,75 @@ function anti(){
       }
     }
     var x = document.getElementsByClassName("analgesics");
-    for (i = 0; i < x.length; i++) {
+      for (i = 0; i < x.length; i++) {
         var y = x[i];
         y.style.display = "none";      
-        }
-
+      }
     var x = document.getElementsByClassName("sedatives");
-    for (i = 0; i < x.length; i++) {
-      var y = x[i];
-
-      y.style.display = "none";
-    }
-
-  var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-  var x = document.getElementsByClassName("other");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-  var x = document.getElementsByClassName("gastro");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-  }
-
+      for (i = 0; i < x.length; i++) {
+        var y = x[i];
+        y.style.display = "none";
+      }
+    var x = document.getElementsByClassName("steroids");
+      for (i = 0; i < x.length; i++) {
+        var y = x[i];
+        y.style.display = "none";
+      }
+    var x = document.getElementsByClassName("other");
+      for (i = 0; i < x.length; i++) {
+        var y = x[i];
+        y.style.display = "none";
+      }
+    var x = document.getElementsByClassName("gastro");
+      for (i = 0; i < x.length; i++) {
+        var y = x[i];
+        y.style.display = "none";
+      }
+}
+//shows analgesic list if not active: hides any others visible, hides if active
 function ana() {
   var x = document.getElementsByClassName("analgesics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-
-    if (y.style.display === "none") {
-      y.style.display = "flex";
-    } else {
-      y.style.display = "none";
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      if (y.style.display === "none") {
+        y.style.display = "flex";
+      } else {
+        y.style.display = "none";
+      }
     }
-  }
   var x = document.getElementsByClassName("antibiotics");
     for (i = 0; i < x.length; i++) {
       var y = x[i];
       y.style.display = "none";
-  }
+    }
 
   var x = document.getElementsByClassName("sedatives");
     for (i = 0; i < x.length; i++) {
       var y = x[i];
       y.style.display = "none";
-  }
-
+    }
   var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   
   var x = document.getElementsByClassName("other");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("gastro");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
 }
-
+//shows sedative list if not active: hides any others visible, hides if active
 function sed() {
   var x = document.getElementsByClassName("sedatives");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
     if (y.style.display === "none") {
       y.style.display = "flex";
     } else {
@@ -92,40 +86,36 @@ function sed() {
     }
   }
   var x = document.getElementsByClassName("antibiotics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("analgesics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("other");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("gastro");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
 }
-
+//shows steroid list if not active: hides any others visible, hides if active
 function ster() {
   var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++) {
     var y = x[i];
-
     if (y.style.display === "none") {
       y.style.display = "flex";
     } else {
@@ -133,40 +123,36 @@ function ster() {
     }
   }
   var x = document.getElementsByClassName("antibiotics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("analgesics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("sedatives");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("other");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("gastro");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
 }
-
+//shows other list if not active: hides any others visible, hides if active
 function othe() {
   var x = document.getElementsByClassName("other");
   for (i = 0; i < x.length; i++) {
     var y = x[i];
-
     if (y.style.display === "none") {
       y.style.display = "flex";
     } else {
@@ -174,35 +160,32 @@ function othe() {
     }
   }
   var x = document.getElementsByClassName("antibiotics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("analgesics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("sedatives");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("gastro");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
 }
-
+//shows gastrointestinal list if not active: hides any others visible, hides if active
 function gast() {
   var x = document.getElementsByClassName("gastro");
   for (i = 0; i < x.length; i++) {
@@ -215,66 +198,37 @@ function gast() {
     }
   }
   var x = document.getElementsByClassName("antibiotics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("analgesics");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("sedatives");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
-
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("steroids");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
   var x = document.getElementsByClassName("other");
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    y.style.display = "none";
-  }
+    for (i = 0; i < x.length; i++) {
+      var y = x[i];
+      y.style.display = "none";
+    }
 }
-
-function info() {
-  if (l.checked) {      //convert pounds to kg
-  }
-  else if (k.checked) {
-  }
-  else {
-    alert("Specify weight type.");
-    return;
-  }
-
-  document.getElementById("dosage").style.display = "flex";
-  document.getElementById("times").style.display = "none";
-  document.getElementById("days").style.display = "none";
-  
-}
-
-
+//hides all items in list
 function reset() {
- document.getElementById("sig1").style.display = "none";
- document.getElementById("sig2").style.display = "none";
- document.getElementById("sig3").style.display = "none";
- document.getElementById("sig4").style.display = "none";
- document.getElementById("sig5").style.display = "none";
- document.getElementById("sig6").style.display = "none";
- document.getElementById("sig7").style.display = "none";
-
+ sig1.style.display = "none";
+ sig2.style.display = "none";
+ sig3.style.display = "none";
+ sig4.style.display = "none";
+ sig5.style.display = "none";
+ sig6.style.display = "none";
 }
-
-(function ($) {
-  $(function () {          
-    $('.modal').modal();
-    $('.trigger-modal').modal();
-  }); 
-})(jQuery); 
